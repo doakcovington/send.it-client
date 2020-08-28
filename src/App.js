3ba178e1c68902f7266f823a97d78d4e1b13d92f
doakcovington <doakcovington@protonmail.com>
2020-08-28 15:58:59 -0700

    Removed bloat

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends React.Component {
-
-  componentDidMount() {
-    fetch("http://localhost:3000/api/vi")
-    .then(res => res.json())
-    .then( 
-      (result) => {
-        console.log(result)
-      })
-  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             We Don't Go To Ravenholm
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );

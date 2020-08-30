f84176d17f8e95b57e3df28a11fa7b7336e7fc2b
doakcovington <doakcovington@protonmail.com>
2020-08-29 18:51:23 -0700

    Built out store

--- src/index.js
+++ src/index.js
@@ -8,10 +8,17 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'redux';
 import { thunk } from 'redux-thunk';
 
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  compose(applyMiddleware(thunk)),
+);
+
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={ store }>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 

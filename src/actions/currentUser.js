c61abdc952942b09e57487ac8e5f8e4b9507a2e9
doakcovington <doakcovington@protonmail.com>
2020-08-31 15:48:37 -0700

    Sets currentUser

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -5,15 +5,23 @@ export const setCurrentUser = user => {
     }
 }
 
-export const login = (credentials, history) => {
+export const login = (credentials) => {
     return dispatch => {
       return fetch("http://localhost:5000/api/v1/login", {
-            credentials: "include",
             method: "POST",
             headers: {
             "Content-Type": "application/json"
             },
             body: JSON.stringify(credentials)
         })
+        .then(res => res.json())
+        .then(user => {
+          if(user.error) {
+            console.log(user.error)
+          } else {
+            dispatch(setCurrentUser(user)) //invokes setCurrentUser action
+          }
+        })
+        .catch(console.log)
     }
 }

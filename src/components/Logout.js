a489d69ec3c8c55884e560c65c45a6f25781c194
doakcovington <doakcovington@protonmail.com>
2020-08-31 19:30:08 -0700

    Built out Logout component

---
+++ src/components/Logout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { logout } from "../actions/currentUser.js"
+
+
+const Logout = ({ logout }) => { //username and password and the props being passed in 
+
+  return (
+      <form onSubmit={handleSubmit}>
+          <input type="submit" value="Log Out"></input>
+      </form>
+  )
+}
+
+export default connect(null, { logout })(Logout) // 'login' is an action creater
